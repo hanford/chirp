3c8b37cdd40d64316cca35355f0ea72adb26415f
hackerman <hackerman@Mehedis-MacBook-Pro.local>
2017-05-24 12:20:44 +0100

    macOS loading issue fix

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-const path = require('path')
 const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
 
 function createWindow() {
     mainWindow = new BrowserWindow({ width: 375, height: 667, vibrancy: 'light' })
 
-    mainWindow.loadURL(path.join(__dirname, "index.html"))
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
+    mainWindow.openDevTools();
     mainWindow.on('closed', () => mainWindow = null)
 }
 
