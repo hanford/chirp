589d8adf1fc7396a6677ffe5a0014e853225fba5
Korbs <KorbsStudio@OnMail.com>
2021-09-11 15:13:56 -0400

    Revamp for Electron 14

--- main.js
+++ main.js
@@ -1,117 +1,92 @@
-const { app, BrowserWindow, Menu, shell } = require('electron')
+const {app, BrowserWindow, Menu, ipcMain, ipcRenderer, nativeImage} = require('electron')
 const path = require('path')
 const url = require('url')
+const Analytics = require('electron-google-analytics')
+new Analytics.default('UA-45226320-3')
 
-const config = require('./config')
-
-function createWindow() {
-  mainWindow = new BrowserWindow({
+function createWindow () {
+  const mainWindow = new BrowserWindow({
     width: 500,
     height: 667,
     icon: path.join(__dirname, './icon/icon.png'),
-    vibrancy: 'dark',
     autoHideMenuBar: true,
-    alwaysOnTop: config.get('alwaysOnTop')
-  })
-
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-    // mainWindow.openDevTools()
-    mainWindow.on('closed', () => mainWindow = null)
-
-    chirpMenu()
-  }
-
-  app.on('ready', createWindow)
-
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
+    webPreferences: {
+      webviewTag: true,
+      nodeIntegration: true,
+      nativeWindowOpen: true // Default changes to true in Electron 15
     }
   })
+  mainWindow.loadFile('index.html')
+}
 
-  app.on('activate', () => {
-    if (mainWindow === null) {
-      createWindow()
-    }
-  })
+app.commandLine.appendSwitch('disable-features', 'CrossOriginOpenerPolicy') // Twitter is blank in webview without this
+app.whenReady().then(() => {createWindow()})
+app.on('window-all-closed', () => {if (process.platform !== 'darwin') {app.quit()}})
+app.on('activate', () => {if (mainWindow === null) {createWindow()}})
 
-  function chirpMenu () {
-    const topBarMenu = [{
-      label: 'Chirp',
-      submenu: [{
-        label: 'About',
-        click(){
-          shell.openExternal('https://jackhanford.com/chirp')
-        }
-      }, {
-        label: 'Get the latest update',
-        click(){
-          shell.openExternal('https://github.com/hanford/chirp/releases')
-        }
-      }, {
-        label: 'Submit a bug report',
-        click(){
-          shell.openExternal('https://github.com/hanford/chirp/issues')
-        }
-      }, {
-        role: 'quit'
-      }]
-    }, {
-      label: 'Edit',
-      submenu: [{
-        role: 'undo'
-      }, {
-        role: 'redo'
-      }, {
-        type: 'separator'
-      }, {
-        role: 'cut'
-      }, {
-        role: 'copy'
-      }, {
-        role: 'paste'
-      }, {
-        role: 'pasteandmatchstyle'
-      }, {
-        role: 'delete'
-      }, {
-        role: 'selectall'
-      }]
-    }, {
-      label: 'View',
-      submenu: [{
-        label: 'Reload',
-        accelerator: 'CmdOrCtrl+R',
-        click (item, focusedWindow) {
-          if (focusedWindow) focusedWindow.reload()
-        }
-      }, {
-        type: 'separator'
-      }, {
-        role: 'resetzoom'
-      }, {
-        role: 'zoomin'
-      }, {
-        role: 'zoomout'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Toggle always on top',
-        accelerator: 'CmdOrCtrl+Shift+A',
-        click () {
-        mainWindow.setAlwaysOnTop(!config.get('alwaysOnTop'))
-        config.set('alwaysOnTop', mainWindow.isAlwaysOnTop())
-        }
-      }, {
-        role: 'hide'
+function chirpMenu () {
+  const topBarMenu = [{
+    label: 'Chirp',
+    submenu: [{
+      label: 'About',
+      click(){
+        shell.openExternal('https://jackhanford.com/chirp')
       }
-    ]
-  }]
+    }, {
+      label: 'Get the latest update',
+      click(){
+        shell.openExternal('https://github.com/hanford/chirp/releases')
+      }
+    }, {
+      label: 'Submit a bug report',
+      click(){
+        shell.openExternal('https://github.com/hanford/chirp/issues')
+      }
+    }, {
+      role: 'quit'
+    }]
+  }, {
+    label: 'Edit',
+    submenu: [{
+      role: 'undo'
+    }, {
+      role: 'redo'
+    }, {
+      type: 'separator'
+    }, {
+      role: 'cut'
+    }, {
+      role: 'copy'
+    }, {
+      role: 'paste'
+    }, {
+      role: 'pasteandmatchstyle'
+    }, {
+      role: 'delete'
+    }, {
+      role: 'selectall'
+    }]
+  }, {
+    label: 'View',
+    submenu: [{
+      label: 'Reload',
+      accelerator: 'CmdOrCtrl+R',
+      click (item, focusedWindow) {
+        if (focusedWindow) focusedWindow.reload()
+      }
+    }, {
+      type: 'separator'
+    }, {
+      role: 'resetzoom'
+    }, {
+      role: 'zoomin'
+    }, {
+      role: 'zoomout'
+    }, {
+      type: 'separator'
+    }
+  ]
+}]
 
-  Menu.setApplicationMenu(Menu.buildFromTemplate(topBarMenu))
+Menu.setApplicationMenu(Menu.buildFromTemplate(topBarMenu))
 }
